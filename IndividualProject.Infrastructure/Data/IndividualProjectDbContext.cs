d1b9a57b5fecdeda9d8311fc4d50407945910891
Daniel Todorov <daniel.todorov@leadconsult.eu>
2024-11-21 12:55:03 +0200

    Added connection string and dbcontext to scope

---
+++ IndividualProject.Infrastructure/Data/IndividualProjectDbContext.cs
@@ -0,0 +1,20 @@
+﻿using IndividualProject.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace IndividualProject.Infrastructure.Data
+{
+    public class IndividualProjectDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<OfficeRoom> OfficeRooms { get; set; }
+        public DbSet<ParkingSpot> ParkingSpots { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+
+        public IndividualProjectDbContext(DbContextOptions<IndividualProjectDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- IndividualProject/Program.cs
+++ IndividualProject/Program.cs
@@ -1,6 +1,12 @@
+using IndividualProject.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<IndividualProjectDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +14,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<IndividualProjectDbContext>();
+    dbContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
